f3f484ac896c50902c9703f5cc77f6943321edb7
Virabyan99 <gmparstone99@gmail.com>
2025-03-29 17:25:50 +0400

    Ui part completed

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { assets } from '@/assets/assets'
+import Message from '@/components/Message'
 import PromptBox from '@/components/PromptBox'
 import Sidebar from '@/components/Sidebar'
 import Image from 'next/image'
@@ -35,7 +36,9 @@ export default function Home() {
             </div>
             <p className='text-sm mt-2'>How can I help you today?</p>
           </>) : (
-            <div></div>
+            <div>
+              <Message role="user" content="What is your name"/>
+            </div>
             )}
             <PromptBox isLoading={isLoading} setIsLoading={setIsLoading}/>
             <p className='text-xs absolute bottom-1 text-gray-500'>AI-generated, for reference only</p>
