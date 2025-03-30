de0d2da5700f5f83d5654d76bece9f453771008b
Virabyan99 <gmparstone99@gmail.com>
2025-03-30 20:55:04 +0400

    Ai endpoints setup done

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import ClientProviders from '@/components/ClientProviders' // Adjust the path if needed
+import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({
   variable: '--font-inter',
@@ -17,6 +18,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <ClientProviders>
+          <Toaster
+            toastOptions={{
+              success: { style: { background: 'black', color: 'white' } },
+              error: { style: { background: 'black', color: 'white' } },
+            }}
+          />
           {children}
         </ClientProviders>
       </body>
