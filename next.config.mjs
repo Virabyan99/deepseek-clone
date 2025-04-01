cbd77a5b604dd8ecb7ae150c27ee3610358707ed
Virabyan99 <gmparstone99@gmail.com>
2025-04-01 22:56:16 +0400

    !!

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      // Only apply the fallback on the client-side, not the server
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback, // Preserve any existing fallbacks
+          crypto: 'crypto-browserify', // Use crypto-browserify as a fallback
+        };
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
